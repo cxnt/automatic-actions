d1800f605ddaa9f7d4d8be20a3eb1a3fcdcd4f64
cxnt <pavelbartsits@gmail.com>
2021-11-16 14:07:39 +0300

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import os
 import setuptools
 import subprocess
+import os
 
 git_repo_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
@@ -9,7 +9,7 @@ git_repo_version = (
 )
 assert "." in git_repo_version
 
-assert os.path.isfile("src/version.py")
+assert os.path.isfile("cf_remote/version.py")
 with open("VERSION", "w", encoding="utf-8") as fh:
     fh.write(f"{git_repo_version}\n")
 
@@ -17,27 +17,26 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="test-package-cxnt",
+    name="automatic-actions",
     version=git_repo_version,
     author="cxnt",
-    author_email="author@example.com",
-    description="A small example package",
+    author_email="cxnt",
+    description="Automatic builds for PYPI and Docker",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cxnt/automatic-actions",
-    project_urls={
-        "Bug Tracker": "https://github.com/cxnt/automatic-actions/issues",
-    },
+    packages=setuptools.find_packages(),
+    package_data={"automatic-actions": ["VERSION"]},
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
+    entry_points={"console_scripts": ["src = src.main:main"]},
     install_requires=[
         "setuptools>=42",
         "wheel"
-        ],
+    ],
 )
