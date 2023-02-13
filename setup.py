f3ced4149fc6e8720aa5a38d2a09c9fdc7f7c461
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2023-02-13 19:24:54 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,24 +53,43 @@ INSTALL_REQUIRES = [
     "numerize",
     "ffmpeg-python==0.2.0",
     "python-multipart==0.0.5",
+    "opencv-python",
 ]
 
-def choose_requirement():
-    opencv_flavors = ["opencv-contrib-python", "opencv-python-headless", "opencv-contrib-python-headless"]
-    chosen = "opencv-python"
-    for flavor in opencv_flavors:
+ALT_INSTALL_REQUIRES = {
+    "opencv-python": ["opencv-python-headless", "opencv-contrib-python", "opencv-contrib-python-headless"],
+}
+
+
+def check_alternative_installation(install_require, alternative_install_requires):
+    """If some version version of alternative requirement installed, return alternative,
+    else return main.
+    """
+    for alternative_install_require in alternative_install_requires:
         try:
-            get_distribution(flavor)
-            chosen = flavor
-            break
+            alternative_pkg_name = re.split(r"[!<>=]", alternative_install_require)[0]
+            get_distribution(alternative_pkg_name)
+            return str(alternative_install_require)
         except DistributionNotFound:
-            pass
-    return str(chosen)
+            continue
+
+    return str(install_require)
 
 
-def get_install_requirements(install_requires):
-    install_requires.append(choose_requirement())
+def get_install_requirements(main_requires, alternative_requires):
+    """Iterates over all install requires
+    If an install require has an alternative option, check if this option is installed
+    If that is the case, replace the install require by the alternative to not install dual package"""
+    install_requires = []
+    for main_require in main_requires:
+        if main_require in alternative_requires:
+            main_require = check_alternative_installation(main_require, alternative_requires.get(main_require))
+        install_requires.append(main_require)
+
     return install_requires
+
+
+INSTALL_REQUIRES = get_install_requirements(INSTALL_REQUIRES, ALT_INSTALL_REQUIRES)
 
 
 setup(
@@ -100,7 +119,7 @@ setup(
         "": ["*.html", "*.css", "*.js"],
         "src": ["video/*.sh", "app/development/*.sh", "imaging/colors.json.gz"],
     },
-    install_requires=get_install_requirements(INSTALL_REQUIRES),
+    install_requires=INSTALL_REQUIRES,
     extras_require={
         "extras": [
             "docker>=5.0.3, <6.0.0",
